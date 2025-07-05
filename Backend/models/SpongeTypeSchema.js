896a8ae76e3855893ccf8b38d7c79c2a7d6da032
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2025-07-05 21:29:56 +0530

    Delete Backend/models directory

--- Backend/models/SpongeTypeSchema.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const SpongeTypeSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String },
-}, { timestamps: true });
-
-module.exports = mongoose.model('SpongeType', SpongeTypeSchema);
